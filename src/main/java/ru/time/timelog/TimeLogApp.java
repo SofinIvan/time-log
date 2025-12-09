42753384efe66dee396540befcf27b758d45d857
isofin <i.sofin@gmail.com>
2025-12-09 19:54:16 +0300

    add project frame

---
+++ src/main/java/ru/time/timelog/TimeLogApp.java
@@ -0,0 +1,11 @@
+package ru.time.timelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLogApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeLogApp.class, args);
+    }
+}
